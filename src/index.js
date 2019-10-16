1a768d2048bbf4cef6fb5938fe135a906623e7dd
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-16 09:52:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  var submitButton = document.getElementById("submit")
+  submitButton.addEventListener("click", function (e) {
+    e.preventDefault()
+    var inputTask = document.getElementById("new-task-description").value
+    var ulListTask = document.getElementById("tasks")
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(inputTask));
+    ulListTask.appendChild(li);
+    document.getElementById("new-task-description").value = ""
+  });
+
+
+
 });
+
